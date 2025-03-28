43aea3e8c0fb1ac9168b5ce7e7d56f0e1831bc68
hjpalpha <75081997+hjpalpha@users.noreply.github.com>
2025-03-28 13:20:13 +0100

    DeployAndProtectTest (#73)

---
+++ lua/wikis/starcraft2/DeployAndProtectTest.lua
@@ -0,0 +1,11 @@
+---
+-- @Liquipedia
+-- wiki=starcraft2
+-- page=Module:DeployAndProtectTest
+--
+-- Please see https://github.com/Liquipedia/Lua-Modules to contribute
+--
+
+local p = {}
+
+return p
