f1de50f766bc3191a6ed177b9835b0bccc24d0e8
hjpalpha <75081997+hjpalpha@users.noreply.github.com>
2025-07-27 18:21:46 +0200

    Update remove_dev.sh

--- scripts/remove_dev.sh
+++ scripts/remove_dev.sh
@@ -79,7 +79,7 @@ searchAndRemove(){
 
   sleep 4
 
-  pages=($(echo "$rawSearchResult" | jq ".query.search.[].title" -r))
+  pages=($(echo "$rawSearchResult" | jq ".query.search.[]" -r))
   echo "::warning::${pages}"
 
   if [[ -n $pages && ${#pages[@]} -ne 0 ]]; then
